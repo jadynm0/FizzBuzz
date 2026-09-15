2a1ea5e7d3ed943b14611530ff26b37b2d4aba9b
jadynm0 <240179201+jadynm0@users.noreply.github.com>
2026-09-15 14:27:00 -0400

    Add Multiples

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Multiples {
+    static void main() {
+        int count = 0;
+
+        for (int i = 1; i <= 1000 ; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
